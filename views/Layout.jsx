f555d0cbe403398ad893ca089d9310674d6cc677
mp1pro <mp1pro@gmail.com>
2019-10-17 17:01:47 -0400

    test dev and aliases

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -101,6 +101,7 @@ class Layout extends React.Component{
                 */}
                 {/*Header Component*/}
                 <Header>
+                
                     
                 </Header>
                 
