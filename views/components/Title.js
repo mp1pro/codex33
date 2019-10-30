f78b6caa7ee3b0a8972458209ba6594f383020a5
mp1pro <mp1pro@gmail.com>
2019-10-30 18:48:37 -0400

    add header title close #2

---
+++ views/components/Title.js
@@ -0,0 +1,14 @@
+ 
+import React from 'react';
+
+class Header extends React.Component{
+    render() {
+        return(
+            <h1>
+                {'CODEX33.COM'}
+            </h1>
+        );
+    }
+}
+
+export default Header;
