b6b742bf80ae91da16bdec5749d4cb3ed4ac3489
mp1pro <mp1pro@gmail.com>
2019-11-05 15:04:36 -0500

    Add clickabe div in nav2

---
+++ views/components/Click_handle.js
@@ -0,0 +1,13 @@
+ 
+ 
+import React from 'react';
+
+class Click_handle extends React.Component{
+    render() {
+        return(
+            <div className = {"click_handle"} ></div>
+        );
+    }
+}
+
+export default Click_handle;
