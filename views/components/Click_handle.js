2ee6238d5b7810ad86816f0d5e0e573b56a32431
mp1pro <mp1pro@gmail.com>
2019-11-06 11:57:33 -0500

    complete clickable slider panel bottom #3

--- views/components/Click_handle.js
+++ views/components/Click_handle.js
@@ -4,8 +4,9 @@ import React from 'react';
 
 class Click_handle extends React.Component{
     render() {
+        
         return(
-            <div className = {"click_handle"} ></div>
+            <div className = {"click_handle"} onClick={this.props.handleSlider}></div>
         );
     }
 }
