4ed39c1f136a52216c6a56e8d4b338e692168099
mp1pro <mp1pro@gmail.com>
2019-11-20 16:59:37 -0500

    add down arrow #4

--- views/components/Click_handle.js
+++ views/components/Click_handle.js
@@ -5,7 +5,11 @@ import React from 'react';
 class Click_handle extends React.Component{
     render() {
         return(
-            <div className = {"click_handle"} onClick={this.props.handleSlider}></div>
+            <div className = {"click_handle"} onClick={this.props.handleSlider}>
+                <div className = {"trapezoid"}>
+                    <div className = {"click_tab"} onClick={this.props.handleSlider}/>
+                </div>
+            </div>
         );
     }
 }
