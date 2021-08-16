596354474d554515422d48156277a782a5098838
mp1pro <mp1pro@gmail.com>
2021-08-16 15:21:34 -0400

    Closes #77 scaled ship to screen resize event

--- views/components/Canvas.js
+++ views/components/Canvas.js
@@ -50,8 +50,8 @@ class Canvas extends React.Component {
 
         this.reset(ctx);
 
-        const w = this.state.width;
-        const h = this.state.height;
+        const w = this.props.width;
+        const h = this.props.height;
 
         const cx = w/2;
         const cy = h/2;
